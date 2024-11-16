51df01de50986fc21a28678bbb251dd83d064994
Joseph Zhong <josephzhong@Josephs-MacBook-Pro.local>
2024-11-16 00:09:12 -0600

    part miniconda3

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 
-apt install python3.9-venv
-python -m venv py39
-pwd
-ls -a -l
-./py39/bin/python -m pip install -r requirements.txt
-./py39/bin/python -m pip install stable-baselines3[extra]
-mkdir models
+cd kick2goal
+../miniconda3/bin/conda create -y -n py39 python=3.9
+../miniconda3/envs/py39/python -m pip install -r requirements.txt
+../miniconda3/envs/py39/python -m pip install stable-baselines3[extra]
 
 # various commands needed to run your job
-./py39/bin/python test_ppo.py $1 $2
-mv tb_log/PPO_1/* ./
-mv models/* ./
+
+mkdir models
+../miniconda3/envs/py39/python test_ppo.py $1 $2
+mv tb_log/PPO_1/* ../
+mv models/* ../
