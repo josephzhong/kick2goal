b18f12c4a80f92039b904d48b41f4018bf248f1d
Joseph Zhong <josephzhong@Josephs-MacBook-Pro.local>
2024-11-15 23:00:57 -0600

    change run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
 python -m venv py39
-source py39/bin/activate
-python -m pip install -r requirements.txt
-python -m pip install stable-baselines3[extra]
+source ./py39/bin/activate;python -m pip install -r requirements.txt
+source ./py39/bin/activate;python -m pip install stable-baselines3[extra]
 mkdir models
 
 # various commands needed to run your job
-python test_ppo.py $1 $2
+source ./py39/bin/activate;python test_ppo.py $1 $2
 mv tb_log/PPO_1/* ./
 mv models/* ./
