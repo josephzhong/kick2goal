09804ad3be6c64c8cef0f84914daa3eaca0d3719
Joseph Zhong <josephzhong@Josephs-MacBook-Pro.local>
2024-11-16 12:04:32 -0600

    add sac

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ cd kick2goal
 mkdir models
 
 # various commands needed to run your job
-../py39/bin/python test_ppo.py $1 $2
-mv tb_log/PPO_1/* ../
+../py39/bin/python $1 $2 $3
+mv tb_log/*_1/* ../
 mv models/* ../
