8941815fccf6167d812d50f9a23c0e8dcafb01c7
Joseph Zhong <josephzhong@Josephs-MacBook-Pro.local>
2024-11-15 23:51:04 -0600

    change back to venv

--- run.sh
+++ run.sh
@@ -1,15 +1,11 @@
 #! /bin/bash
 
-mkdir miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda3/miniconda.sh
-bash miniconda3/miniconda.sh -b -u -p miniconda3
-rm miniconda3/miniconda.sh
-miniconda3/bin/conda create -y -n py39 python=3.9
-miniconda3/envs/py39/bin/python -m pip install -r requirements.txt
-miniconda3/envs/py39/bin/python -m pip install stable-baselines3[extra]
+python -m venv py39
+./py39/bin/python -m pip install -r requirements.txt
+./py39/bin/python -m pip install stable-baselines3[extra]
 mkdir models
 
 # various commands needed to run your job
-miniconda3/envs/py39/bin/python test_ppo.py $1 $2
+./py39/bin/python test_ppo.py $1 $2
 mv tb_log/PPO_1/* ./
 mv models/* ./
