f963b0bd7aa14c3b4dbc7ec8c5d674996e14a5cb
Joseph Zhong <josephzhong@Josephs-MacBook-Pro.local>
2024-11-15 23:07:53 -0600

    add sudo

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-python -m venv py39
-source ./py39/bin/activate;python -m pip install -r requirements.txt
-source ./py39/bin/activate;python -m pip install stable-baselines3[extra]
+sudo python -m venv py39
+sudo source ./py39/bin/activate
+sudo python -m pip install -r requirements.txt
+sudo python -m pip install stable-baselines3[extra]
 mkdir models
 
 # various commands needed to run your job
-source ./py39/bin/activate;python test_ppo.py $1 $2
+python test_ppo.py $1 $2
 mv tb_log/PPO_1/* ./
 mv models/* ./
