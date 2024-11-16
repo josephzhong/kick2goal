40884ed884fbffa43994c54f847fbdabf3a92f21
Joseph Zhong <josephzhong@Josephs-MacBook-Pro.local>
2024-11-15 22:23:15 -0600

    add local venv

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 
 # various commands needed to run your job
-./py39/bin/python test_ppo.py $1 $2
-mv tb_log/PPO_1/* ./
-mv models/* ./
+./py39/bin/python kick2goal/test_ppo.py $1 $2
+mv kick2goal/tb_log/PPO_1/* ./
+mv kick2goal/models/* ./
