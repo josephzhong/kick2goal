b24151864e7ef25b0faa2fb2da80baaf3ee78b25
Joseph Zhong <josephzhong@Josephs-MacBook-Pro.local>
2024-11-15 23:19:13 -0600

    try miniconda

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
-sudo python -m venv py39
-sudo source ./py39/bin/activate
-sudo python -m pip install -r requirements.txt
-sudo python -m pip install stable-baselines3[extra]
+mkdir miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda3/miniconda.sh
+bash miniconda3/miniconda.sh -b -u -p miniconda3
+rm miniconda3/miniconda.sh
+miniconda3/bin/conda create -y -n py39 python=3.9
+miniconda3/envs/py39/bin/python -m pip install -r requirements.txt
+miniconda3/envs/py39/bin/python -m pip install stable-baselines3[extra]
 mkdir models
 
 # various commands needed to run your job
-python test_ppo.py $1 $2
+miniconda3/envs/py39/bin/python test_ppo.py $1 $2
 mv tb_log/PPO_1/* ./
 mv models/* ./
