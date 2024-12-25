fbb69d8726c98c1d7c259f368f3a619e173c58f9
Joseph Zhong <josephzhong@Josephs-MacBook-Pro.local>
2024-12-25 01:39:37 -0600

    add difficult

--- evaluate_pretrain.py
+++ evaluate_pretrain.py
@@ -86,3 +86,33 @@ if __name__ == "__main__":
         f.write(",".join([f"{rew:.2f}" for rew in rews]) + "\n")
         f.write(",".join([f"{goal}" for goal in infors['goal']])+ "\n")
         f.write(",".join([f"{infors['init_state_x'][index]:.2f};{infors['init_state_y'][index]:.2f}" for index in range(len(infors['init_state_x']))]) + "\n")
+
+    print("test difficult")
+    test_env_difficult = make_vec_env(KickToGoalGym, n_envs=config["eval_num_envs"],
+                                      vec_env_cls=SubprocVecEnv, vec_env_kwargs={"start_method": "fork"},
+                                      env_kwargs={"episode_length": 10000,
+                                                  "varying_init_state": True,
+                                                  "goal_reward": 1000})
+    for env_idx in range(test_env_difficult.num_envs):
+        test_env_difficult.env_method("reset_seed", seed=config["test_seed"] + env_idx, indices=env_idx)
+        test_env_difficult.env_method("update_attribute", attribute_name="init_ball_to_goal_distance_score",
+                                      value=0.95, indices=env_idx)
+        # new_delta = callback.last_delta
+        # eval_env.env_method("change_goal_position", new_delta=new_delta, indices=env_idx)
+
+    # models.exploration_final_eps = 0.01
+    rews, lengths, infors = evaluate_policy(model, test_env_difficult, n_eval_episodes=config["num_of_eval_episodes"],
+                                            return_episode_rewards=True,
+                                            infor_keys=["goal", "init_state_x", "init_state_y"], deterministic=True)
+    print("difficult test result:")
+    print(f"Number of difficult test episodes {len(rews)}")
+    print(f"IQM of difficult test rewards {interquartile_mean(rews):.4f}")
+    print(f"Mean of difficult test rewards {np.mean(rews):.4f}")
+    print(f"Std of difficult test rewards {np.std(rews):.4f}")
+    print(f"IQM of difficult test game_lens {interquartile_mean(lengths):.4f}")
+    print(f"goal difficult test ratio {np.mean(infors['goal']):.4f}")
+
+    with open(f"{config['test_seed']}_{config['policy_kwargs']['maturity_threshold']}_{config['test_seed']}_difficult.rewards", "w") as f:
+        f.write(",".join([f"{rew:.2f}" for rew in rews]) + "\n")
+        f.write(",".join([f"{goal}" for goal in infors['goal']]) + "\n")
+        f.write(",".join([f"{infors['init_state_x'][index]:.2f};{infors['init_state_y'][index]:.2f}" for index in range(len(infors['init_state_x']))]) + "\n")
